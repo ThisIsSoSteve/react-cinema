45c3a733f161809e50db974c241f3ba5ae5c2410
ThisIsSoSteve <steve.caldwell22@gmail.com>
2019-10-18 20:03:34 +0100

    added basic call to api for testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://localhost:44372/movie/featured`)
+      .then(res => {
+        console.log(res);
+        
+        const movies = res.data;
+        this.setState({ movies });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+         <h1>Cinema UI</h1>
+         <ul>
+        { this.state.movies.map(movie => <li key={movie.id}>{movie.title}</li>)}
+        </ul>
+       </header>
+     </div>
+    )
+  }
+}
